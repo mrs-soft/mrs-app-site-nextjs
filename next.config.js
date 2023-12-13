6b4226bffc548a4c5c92dea1dd0b01634cea77a0
KaterinaZv <katuha.hip.hop@gmail.com>
2023-12-13 17:43:01 +0300

    Fix base path

--- next.config.js
+++ next.config.js
@@ -2,13 +2,12 @@
  * @type {import('next').NextConfig}
  */
 
-
-const isProd = process.env.NODE_ENV === "production"
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  basePath: isProd ? 'mrs-app-site-nextjs' : '',
-  output: 'export',
-  distDir: 'dist',
-}
+  basePath: isProd ? "" : "",
+  output: "export",
+  distDir: "dist",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
