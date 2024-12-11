cfdfc5db9bdaacc46380a965375349e2677b91d4
Anton Ivakov <aivakov@plotpad.ru>
2024-12-11 16:47:27 +0300

    clear code

--- src/components/widget/widget.tsx
+++ src/components/widget/widget.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react"
 import "saas-shop-widget/dist/main.css"
 
+const WIDGET_CLASS = "saas_shop_tariffs"
+
 interface IWidget {
   className: string
   productId: string
@@ -19,15 +21,15 @@ export const Widget = ({className, productId}: IWidget) => {
         api: process.env.NEXT_PUBLIC_SAAS_SHOP_API || "",
       }
       const NewWidget = new WidgetSaasShopPresenter({
-        idContainer: '.saas_shop_tariffs',
+        idContainer: `.${WIDGET_CLASS}`,
         api: config.api,
         redirectUrl: config.redirectUrl,
-        productId: '710d6023-0945-4060-a573-68e4527590fa',
+        productId: "710d6023-0945-4060-a573-68e4527590fa",
         // productId: config.productId,
       })
       NewWidget.init(config)
   }
 
 
-  return <div className={`saas_shop_tariffs ${className}`}></div>
+  return <div className={`${WIDGET_CLASS} ${className}`}></div>
 }
